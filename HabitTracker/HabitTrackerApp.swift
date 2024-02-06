7c07632de4ca4cf34be7bee9b65160f43a4b1a38
anousheh21 <anousheh1@gmail.com>
2024-02-06 16:57:18 +0000

    first commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Anousheh Hasan on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
